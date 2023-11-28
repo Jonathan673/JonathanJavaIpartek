237b9aac0dfbbc208b5636e2ef3175e53cc90372
Jonathan673 <jonathan.camac@gmail.com>
2023-11-28 12:39:54 +0100

    [ADD] Floristeria web hecha pero con errores de visualizacion

--- floristeria/src/main/java/com/floristeria/dataaccess/Dao.java
+++ floristeria/src/main/java/com/floristeria/dataaccess/Dao.java
@@ -1,11 +1,23 @@
 package com.floristeria.dataaccess;
 
-import com.floristeria.entities.Producto;
-
 public interface Dao<T> {
-	Iterable<T> ObtenerTodosLosProductos();
-	T ObtenerPorId(Long id);
-	T InsertarProducto(Producto producto);
-	T ModificarProducto(Producto producto);
-	void EliminarProducto(Long id);
+	default Iterable<T> ObtenerTodosLosProductos() {
+		throw new AccesoDatosException("NO IMPLEMENTADO");
+	}
+
+	default T ObtenerPorId(Long id) {
+		throw new AccesoDatosException("NO IMPLEMENTADO");
+	}
+
+	default T InsertarProducto(T objeto) {
+		throw new AccesoDatosException("NO IMPLEMENTADO");
+	}
+
+	default T ModificarProducto(T objeto) {
+		throw new AccesoDatosException("NO IMPLEMENTADO");
+	}
+
+	default void EliminarProducto(Long id) {
+		throw new AccesoDatosException("NO IMPLEMENTADO");
+	}
 }
--- floristeria/src/main/java/com/floristeria/globals/Global.java
+++ floristeria/src/main/java/com/floristeria/globals/Global.java
@@ -1,11 +1,13 @@
 package com.floristeria.globals;
 
-//import com.floristeria.businesslogic.AdminNegocio;
+import com.floristeria.businesslogic.AdminNegocio;
+import com.floristeria.businesslogic.UsuarioNegocio;
+
 
 
 public class Global {
 	public static final Fabrica FABRICA = new FabricaImpl("C:/sqlite/floristeria.properties");
 	
-//	public static final UsuarioNegocio UN = FABRICA.obtenerUsuarioNegocio();
-//	public static final AdminNegocio AN = FABRICA.obtenerAdminNegocio();
+	public static final UsuarioNegocio UN = FABRICA.obtenerUsuarioNegocio();
+	public static final AdminNegocio AN = FABRICA.obtenerAdminNegocio();
 }
