4089623cfea1158fa176367263b10152d0be33fe
Jonathan673 <jonathan.camac@gmail.com>
2023-12-01 12:37:50 +0100

    [UPDATE] Ejercicio del profesor con maven, hibernate, nuevos metodos mas comprimidos, anotaciones

--- amazonia2/src/main/java/com/amazonia2/presentacion/consola/PresentacionConsola.java
+++ amazonia2/src/main/java/com/amazonia2/presentacion/consola/PresentacionConsola.java
@@ -1,16 +1,47 @@
 package com.amazonia2.presentacion.consola;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Set;
+
 import com.amazonia2.entidades.Producto;
+import com.amazonia2.entidades.Rol;
 import com.amazonia2.globales.Global;
-import com.amazonia2.logicanegocio.UsuarioNegocio;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 
 public class PresentacionConsola {
 
 	public static void main(String[] args) {
-		UsuarioNegocio un = Global.FABRICA.obtenerUsuarioNegocio(); 
-		
-		for(Producto p: un.listadoProductos()) {
+		for(Producto p: Global.UN.listadoProductos()) {
 			System.out.println(p);
+		}
+		
+		for(Rol r: Global.AN.obtenerTodosLosRoles()) {
+			System.out.println(r);
+			
+//			for(Usuario u: r.getUsuarios()) {
+//				System.out.println(u);
+//			}
+		}
+		
+		Producto producto = Producto.builder().codigoBarras("1234567890").nombre("aa").precio(new BigDecimal("10")).unidades(5).fechaCaducidad(LocalDate.now().plusYears(1)).build();
+		
+		ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+		Validator validator = validatorFactory.getValidator();
+		
+		Set<ConstraintViolation<Producto>> errores = validator.validate(producto);
+		
+		for(ConstraintViolation<Producto> error: errores) {
+			System.out.println("El campo " + error.getPropertyPath() + " " + error.getMessage());
+		}
+		
+		if(errores.size() == 0) {
+			System.out.println("El producto es válido");
+			System.out.println(producto);
 		}
 	}
 
