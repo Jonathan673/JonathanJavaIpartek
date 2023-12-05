664b1840ede1e5c46fabc78ef0332e1d2dd08147
Jonathan673 <jonathan.camac@gmail.com>
2023-12-05 10:28:21 +0100

    [UPDATE] Reducción de código

--- floristeria/src/main/java/com/floristeria/entities/Carrito.java
+++ floristeria/src/main/java/com/floristeria/entities/Carrito.java
@@ -3,74 +3,43 @@ package com.floristeria.entities;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Carrito {
 	private Long id;
 	private Usuario usuario;
-	
+
 	private Map<Long, Producto> productos = new HashMap<>();
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-
+	// getter and setter
 	public Set<Producto> getProductos() {
 		return new HashSet<Producto>(productos.values());
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, productos, usuario);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Carrito other = (Carrito) obj;
-		return Objects.equals(id, other.id) && Objects.equals(productos, other.productos)
-				&& Objects.equals(usuario, other.usuario);
-	}
-
-	@Override
-	public String toString() {
-		return "Carrito [id=" + id + ", usuario=" + usuario + ", productos=" + productos + "]";
-	}
-	
+	// metodos internos
 	public void addProducto(Producto producto) {
 		productos.put(producto.getId(), producto);
 	}
-	
+
 	public void updateUnidades(Long id, Integer unidades) {
 		productos.get(id).setUnidades(unidades);
 	}
-	
+
 	public void removeProducto(Long id) {
 		productos.remove(id);
 	}
-	
+
 	public Producto getProducto(Long id) {
 		return productos.get(id);
 	}
-	
+
 	public int getNumeroProductos() {
 		return productos.size();
 	}
--- floristeria/src/main/java/com/floristeria/entities/Producto.java
+++ floristeria/src/main/java/com/floristeria/entities/Producto.java
@@ -3,82 +3,42 @@ package com.floristeria.entities;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Producto {
-	// productos que estarán disponibles en la floristeria, necesita un id, nombre,
-	// unidades disponibles y tipo de producto (flor artificial,
-	// complemento tipo maceta...), precio
 	private Long id;
 	private String nombre;
 	private String productTipe;
 	private BigDecimal precio;
 	private Integer unidades;
-
 	
-	//Constructores
-	public Producto(Long id, String nombre, String tipoProducto, BigDecimal precio, Integer unidades) {
-		setId(id);
-		setNombre(nombre);
-		setProductTipe(tipoProducto);
-		setPrecio(precio);
-		setUnidades(unidades);
-	}
-	public Producto( String nombre, String tipoProducto, BigDecimal precio, Integer unidades) {
+	//Constructor propio
+	public Producto(String nombre, String tipoProducto, BigDecimal precio, Integer unidades) {
 		this(null, nombre, tipoProducto, precio, unidades);
 	}
 
-	public Producto() {
-		
-	}
-	//getters and setters
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public Integer getUnidades() {
-		return unidades;
-	}
-
+	//Getters and setters propios
 	public void setUnidades(Integer unidades) {
-		if(unidades<=0 || unidades == null) {
+		if (unidades <= 0 || unidades == null) {
 			this.unidades = 0;
 			return;
 		}
 		this.unidades = unidades;
 	}
 
-	public String getProductTipe() {
-		return productTipe;
-	}
-
 	public void setProductTipe(String tipoProducto) {
-		if(tipoProducto == null) {
+		if (tipoProducto == null) {
 			tipoProducto = "Complemento";
 		}
 		this.productTipe = tipoProducto;
 	}
 
-	public BigDecimal getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(BigDecimal precio) {
-		this.precio = precio;
-	}
-
-	//HashCode y equals
+	// HashCode y equals propios
 	@Override
 	public int hashCode() {
 		return Objects.hash(id, nombre, productTipe);
@@ -96,9 +56,4 @@ public class Producto {
 		return id == other.id && Objects.equals(nombre, other.nombre) && productTipe == other.productTipe;
 	}
 
-	@Override
-	public String toString() {
-		return "Productos [id=" + id + ", nombre=" + nombre + ", unidades=" + unidades + ", productTipe=" + productTipe
-				+ ", precio=" + precio + "]";
-	}
 }
