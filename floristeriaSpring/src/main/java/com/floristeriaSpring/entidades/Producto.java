843fe5920bde6aabdd7f97cbde4a2012ce00e082
Jonathan673 <jonathan.camac@gmail.com>
2023-12-13 11:34:42 +0100

    [ADD] Proyecto desde cero porque el otro ha petado sin razon aparente, y no puedo abrirlo, no lo detecta

---
+++ floristeriaSpring/src/main/java/com/floristeriaSpring/entidades/Producto.java
@@ -0,0 +1,61 @@
+package com.floristeriaSpring.entidades;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Producto {
+	
+	private Long id;
+	private String nombre;
+	private String productTipe;
+	private BigDecimal precio;
+	private Integer unidades;
+	
+	//Constructor propio
+	public Producto(String nombre, String tipoProducto, BigDecimal precio, Integer unidades) {
+		this(null, nombre, tipoProducto, precio, unidades);
+	}
+
+	//Getters and setters propios
+	public void setUnidades(Integer unidades) {
+		if (unidades <= 0 || unidades == null) {
+			this.unidades = 0;
+			return;
+		}
+		this.unidades = unidades;
+	}
+
+	public void setProductTipe(String tipoProducto) {
+		if (tipoProducto == null) {
+			tipoProducto = "Complemento";
+		}
+		this.productTipe = tipoProducto;
+	}
+
+	// HashCode y equals propios
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nombre, productTipe);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Producto other = (Producto) obj;
+		return id == other.id && Objects.equals(nombre, other.nombre) && productTipe == other.productTipe;
+	}
+}
---
+++ floristeriaSpring/src/test/java/com/floristeriaSpring/FloristeriaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floristeriaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloristeriaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
