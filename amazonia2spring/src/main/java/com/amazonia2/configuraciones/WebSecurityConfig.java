47e3e79adc2b1bae844876cced8ae6190c3482af
Jonathan673 <jonathan.camac@gmail.com>
2023-12-18 11:44:43 +0100

    amazonia2

---
+++ amazonia2spring/src/main/java/com/amazonia2/configuraciones/WebSecurityConfig.java
@@ -0,0 +1,93 @@
+package com.amazonia2.configuraciones;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+	// https://bcrypt-generator.com/
+	
+	// CODIFICACIÓN DE CONTRASEÑAS BCRYPT
+	@Bean
+	PasswordEncoder passwordEncoder() {
+	    return NoOpPasswordEncoder.getInstance(); //new BCryptPasswordEncoder(12);
+	}
+	
+	// AUTENTICACIÓN
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth, DataSource dataSource)
+	  throws Exception {
+	    auth.jdbcAuthentication().dataSource(dataSource)
+	    	.usersByUsernameQuery("""
+	    			SELECT email,password,1
+	    			FROM usuarios
+	    			WHERE email = ?
+	    			""")  
+	    	.authoritiesByUsernameQuery("""
+	    			SELECT u.email, CONCAT('ROLE_', r.nombre) 
+	    			FROM usuarios u
+	    			JOIN roles r ON u.rol_id = r.id
+	    			WHERE email = ?
+	    			""");
+	}
+	
+//	@Bean
+//	UserDetailsService userDetailsService() {
+//		UserDetails admin = User.withDefaultPasswordEncoder()
+//			.username("javier").password("contra").roles("ADMIN")
+//			.build();
+//		UserDetails user = User.withDefaultPasswordEncoder()
+//			.username("pepe").password("perez").roles("USER")
+//			.build();
+//
+//		return new InMemoryUserDetailsManager(user, admin);
+//	}
+
+	// AUTORIZACIÓN
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+	        .authorizeHttpRequests(requests -> requests
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/detalle").authenticated()
+                .anyRequest().permitAll()
+//				.requestMatchers("/", "/home").permitAll()
+//				.anyRequest().authenticated()
+	        )
+	        .exceptionHandling(handling -> handling.accessDeniedPage("/login?noautorizado"))
+	        .formLogin(form -> form
+                .loginPage("/login")
+                .permitAll()
+	        )
+	        .logout(logout -> logout.permitAll());
+
+		return http.build();
+	}
+	
+//		AUTENTICACIÓN DE MYSQL
+//		----------------------
+//		create table users(
+//			username varchar(50) not null primary key,
+//			password varchar(500) not null,
+//			enabled boolean not null
+//		);
+//
+//		create table authorities (
+//			username varchar(50) not null,
+//			authority varchar(50) not null,
+//			constraint fk_authorities_users foreign key(username) references users(username)
+//		);
+//		create unique index ix_auth_username on authorities (username,authority);
+
+}
--- amazonia2spring/src/main/java/com/amazonia2/presentacion/controladores/IndexController.java
+++ amazonia2spring/src/main/java/com/amazonia2/presentacion/controladores/IndexController.java
@@ -1,19 +1,28 @@
 package com.amazonia2.presentacion.controladores;
 
+import java.util.logging.Level;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.amazonia2.bibliotecas.Alerta;
 import com.amazonia2.logicanegocio.UsuarioNegocio;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.java.Log;
+
+@Log
 @Controller
 @RequestMapping
-public class IndexController {
+public class IndexController implements ErrorController {
 	@Autowired
 	private UsuarioNegocio negocio;
-	
+
 //	@ResponseBody
 	@GetMapping("/")
 	public String index(Model modelo) {
@@ -21,11 +30,47 @@ public class IndexController {
 		modelo.addAttribute("productos", negocio.listadoProductos());
 		return "index";
 	}
-	
+
 //	@ResponseBody
 	@GetMapping("/detalle")
 	public String detalle(Model modelo, Long id) {
 		modelo.addAttribute("producto", negocio.detalleProducto(id));
 		return "detalle";
+	}
+
+	@GetMapping("/login")
+	public String login(String error, String logout, String noautorizado, String interactivo, Model modelo) {
+		Alerta alerta = new Alerta(modelo);
+
+		if (error != null) {
+			alerta.danger("El usuario o la contraseña no son correctos");
+		} else if (logout != null) {
+			alerta.success("Se ha desconectado de la sesión correctamente");
+		} else if(noautorizado != null) {
+			alerta.danger("Tu nivel de acceso no es suficiente");
+		} else if(interactivo != null) {
+		} else {
+			alerta.warning("Tienes que iniciar sesión");
+		}
+
+		return "login";
+	}
+
+	@GetMapping("/error")
+	public String error(Model modelo, HttpServletRequest request, Exception exception) {
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		log.severe("Status: " + status.toString());
+
+		do {
+			log.severe("Tipo: " + exception.getClass().getSimpleName());
+			log.severe("Mensaje: " + exception.getMessage());
+			log.log(Level.SEVERE, "Detalles de la excepción", exception);
+			
+			exception = (Exception) exception.getCause();
+		} while(exception != null);
+
+		modelo.addAttribute("status", status);
+
+		return "_error";
 	}
 }
