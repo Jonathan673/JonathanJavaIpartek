b5c561247b388b0cfa2a46a91911508d870e9b58
Jonathan673 <jonathan.camac@gmail.com>
2023-12-22 13:04:33 +0100

    Proyecto de prueba desde cero inmobiliaria

---
+++ inmoviliariaPruebaSpring/src/main/java/com/inmoviliariaPruebaSpring/entidades/Establecimiento.java
@@ -0,0 +1,40 @@
+package com.inmoviliariaPruebaSpring.entidades;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "establecimientos")
+public class Establecimiento {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotNull
+	@Size(min = 2, max = 50)
+	private String nombre;
+	@NotNull
+	@Size(min = 2, max = 100)
+	private String ubicacion;
+	@NotNull
+	@Size(min = 2, max = 50)
+	private String tipo_establecimiento;
+	@NotNull
+	@Min(0)
+	private BigDecimal precio;
+}
