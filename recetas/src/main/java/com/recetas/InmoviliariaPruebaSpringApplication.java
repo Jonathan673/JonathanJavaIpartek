5cd4352c11ebd6ca6e4083427996552422209398
Jonathan673 <jonathan.camac@gmail.com>
2024-01-08 12:46:33 +0100

    [ADD] Proyecto Recetas

---
+++ recetas/src/main/java/com/recetas/InmoviliariaPruebaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.recetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmoviliariaPruebaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmoviliariaPruebaSpringApplication.class, args);
+	}
+
+}
