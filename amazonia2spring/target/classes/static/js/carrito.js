866ecd74a61b78d478f23e27a914b697531edb8a
Jonathan673 <jonathan.camac@gmail.com>
2024-01-08 12:39:03 +0100

    [ADD] Todo lo nuevo desde el inicio del curso

---
+++ amazonia2spring/target/classes/static/js/carrito.js
@@ -0,0 +1,37 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', function() {
+	const menos = document.querySelectorAll('.menos');
+	const mas = document.querySelectorAll('.mas');
+	
+	for(const boton of menos) {
+		boton.addEventListener('click', clickMenos);
+	}
+	
+	mas.forEach(boton => boton.addEventListener('click', clickMas));
+});
+
+function clickMas() {
+	const botonMas = this;
+	
+	const div = botonMas.parentNode;
+	
+	const input = div.querySelector('input');
+	
+	input.value++;
+	
+	const botonMenos = div.querySelector('.menos');
+	
+	botonMenos.disabled = false; 
+}
+
+function clickMenos() {
+	const boton = this;
+	const input = boton.parentNode.querySelector('input');
+	
+	input.value--;
+	
+	if(input.value == 0) {
+		boton.disabled = true;
+	}
+}
